a483db72004f4dabbb633fae7a39e3588fa9d372
68000a <44293414+68000a@users.noreply.github.com>
2019-06-15 11:12:31 +0200

    3sat Mediathek - 1.3.2 Bis auf Weiteres nicht verfügbar

--- code/plugin.video.3satmediathek/default.py
+++ code/plugin.video.3satmediathek/default.py
@@ -1,4 +1,14 @@
 ﻿# -*- coding: utf-8 -*-
 from lib3sat import *
 import xbmc
-list()
+import xbmcgui
+import libmediathek3 as libMediathek
+
+# war: list()
+
+libMediathek.endOfDirectory()
+dialog = xbmcgui.Dialog()
+title = "3sat Mediathek"
+text = "Aufgrund von Änderungen an der 3sat Website ist die 3sat Mediathek für Kodi bis auf Weiteres nicht verfügbar."
+dialog.ok(title, text)
+xbmc.executebuiltin("XBMC.ActivateWindow(Home)")
