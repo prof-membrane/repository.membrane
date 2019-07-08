8b444a957977bed96a75de497af0dce6580760b4
68000a <44293414+68000a@users.noreply.github.com>
2019-07-08 17:34:24 +0200

    lib3sat 1.2.4 Performance-Verbesserungen

--- code/script.module.lib3sat/lib/lib3sathtmlparser.py
+++ code/script.module.lib3sat/lib/lib3sathtmlparser.py
@@ -2,7 +2,7 @@
 import time
 import json
 # import xbmc
-import BeautifulSoup as bs
+import bs4 as bs
 import libmediathek3 as libMediathek
 from datetime import date, datetime, timedelta
 
@@ -64,37 +64,48 @@ def lib3satHtmlListDate():
 
 def chooseImage(pictureList, thumbnail_type):
 	if not (pictureList is None):
-		pictureList = (
-			filter(
-				lambda(x):
-					hasattr(x,'attrs')
-					and
-					len(filter(lambda(y): (y[0]=='class') and (y[1].find(thumbnail_type[preferred_thumbnail_type])>=0), x.attrs)) > 0
-				, pictureList
-			)
-		)
-		if len(pictureList) > 0:
-			pictureSources = filter(lambda(x): x[0]=='data-srcset', pictureList[0].attrs)
-			if len(pictureSources) > 0:
-				pictures = pictureSources[0][1].split(',')
-				for index, item in enumerate(pictures):
-					pictures[index] = item.split(' ')
-				for resolution in preferred_resolutions[preferred_thumbnail_type]:
-					for picture in pictures:
-						if len(picture) > 1 and picture[1] == resolution:
-							return picture[0]
-			pictureSources = filter(lambda(x): x[0]=='data-src', pictureList[0].attrs)
-			if len(pictureSources) > 0:
-				return pictureSources[0][1]
+		for pictureItem in pictureList:
+			if hasattr(pictureItem,'attrs') and (thumbnail_type[preferred_thumbnail_type] in pictureItem.attrs.get('class', [])):
+				pictureSources = pictureItem.attrs.get('data-srcset', None)
+				if not (pictureSources is None):
+					pictures = pictureSources.split(',')
+					for index, item in enumerate(pictures):
+						pictures[index] = item.split(' ')
+					for resolution in preferred_resolutions[preferred_thumbnail_type]:
+						for picture in pictures:
+							if len(picture) > 1 and picture[1] == resolution:
+								return picture[0]
+				# Fallback
+				pictureSource = pictureItem.attrs.get('data-src', None)
+				return pictureSource
 	return None
+
+
+def str_to_airedtime(airedtime_str):
+	if not airedtime_str:	# check for None or empty string
+		return None
+	start = airedtime_str.split('+')
+	zulutime = (len(start) == 1)
+	if zulutime:
+		format = '%Y-%m-%dT%H:%M:%SZ'
+	else:
+		format = '%Y-%m-%dT%H:%M:%S'
+	try:
+		airedtime = datetime.strptime(start[0], format)
+	except TypeError:
+		airedtime = datetime(*(time.strptime(start[0], format)[0:6]))
+	if zulutime:
+		tz_offset = timedelta (minutes = (time.timezone / -60) + (time.localtime().tm_isdst * 60))
+		airedtime += tz_offset
+	return airedtime
 
 
 def getDate(date_str):
 	l = []
 	url = base + '/programm?airtimeDate=' + date_str
 	response = libMediathek.getUrl(url)
-	soup = bs.BeautifulSoup(response)
-	articles = soup.findAll('article', {'class': (lambda(x): x.find('is-video')>=0)})
+	soup = bs.BeautifulSoup(response, 'html.parser')
+	articles = soup.findAll('article', {'class': 'is-video'})
 	for article in articles:
 		d = {}
 		name = article.find('h3')
@@ -102,32 +113,14 @@ def getDate(date_str):
 			d['_type'] = 'video'
 			d['mode'] = 'lib3satHtmlPlay'
 			d['_name'] = name.text
-			airedtime_begin = None
-			airedtime_end = None
-			for attr in article.attrs:
-				if attr[0] == 'data-airtime-begin' or attr[0] == 'data-airtime-end':
-					start = attr[1].split('+')
-					zulutime = (len(start) == 1)
-					if zulutime:
-						format = '%Y-%m-%dT%H:%M:%SZ'
-					else:
-						format = '%Y-%m-%dT%H:%M:%S'
-					try:
-						airedtime = datetime.strptime(start[0], format)
-					except TypeError:
-						airedtime = datetime(*(time.strptime(start[0], format)[0:6]))
-					if zulutime:
-						tz_offset = timedelta (minutes = (time.timezone / -60) + (time.localtime().tm_isdst * 60))
-						airedtime += tz_offset
-					if attr[0] == 'data-airtime-begin':
-						airedtime_begin = airedtime
-						airedtime = datetime (airedtime.year, airedtime.month, airedtime.day, airedtime.hour, (airedtime.minute / 5) * 5)
-						d['_airedtime'] = airedtime.strftime('%H:%M')
-						d['_name'] = '(' + d['_airedtime'] + ') ' + d['_name']
-					else:
-						airedtime_end = airedtime
-			if not (airedtime_begin is None) and not (airedtime_end is None):
-				d['duration'] = str((airedtime_end - airedtime_begin).seconds)
+			airedtime_begin = str_to_airedtime(article.attrs.get('data-airtime-begin', None))
+			if not (airedtime_begin is None):
+				airedtime = datetime (airedtime_begin.year, airedtime_begin.month, airedtime_begin.day, airedtime_begin.hour, (airedtime_begin.minute / 5) * 5)
+				d['_airedtime'] = airedtime.strftime('%H:%M')
+				d['_name'] = '(' + d['_airedtime'] + ') ' + d['_name']
+				airedtime_end = str_to_airedtime(article.attrs.get('data-airtime-end', None))
+				if not (airedtime_end is None):
+					d['duration'] = str((airedtime_end - airedtime_begin).seconds)
 			plot = article.find('p', {'class': 'teaser-epg-text'})
 			if not (plot is None):
 				d['_plot'] = plot.text
@@ -135,8 +128,10 @@ def getDate(date_str):
 			if not (picture is None):
 				d['_thumb'] = chooseImage(picture.contents, thumbnail1_types)
 			url = article.find('a', {'data-link': (lambda(x): not(x is None))})
-			if not (url is None):
-				d['url'] = base + filter(lambda(x): x[0]=='href', url.attrs)[0][1]
+			if not (url is None) and not (url.attrs is None):
+				href = url.attrs.get('href', None)
+				if not (href is None):
+					d['url'] = base + href
 			l.append(d)
 	return l
 
@@ -155,17 +150,16 @@ def lib3satHtmlListDateVideos():
 def getAZ(url):
 	l = []
 	response = libMediathek.getUrl(url)
-	soup = bs.BeautifulSoup(response)
+	soup = bs.BeautifulSoup(response, 'html.parser')
 	articles = soup.findAll('article')
 	for article in articles:
 		d = {}
-		name_link = article.find('a',  {'class': (lambda(x): x.find('teaser-title-link')>=0)})
-		if not (name_link is None):
-			href_attr = filter(lambda(x): x[0] == 'href', name_link.attrs)
-			name_attr = article.find('p',  {'class': (lambda(x): x.find('a--headline')>=0)})
-			if len(name_attr) > 0 and len(href_attr) > 0:
+		name_link = article.find('a',  {'class': 'teaser-title-link'})
+		if not (name_link is None) and not (name_link.attrs is None):
+			href = name_link.attrs.get('href', None)
+			name_attr = article.find('p',  {'class': 'a--headline'})
+			if len(name_attr) > 0 and not (href is None):
 				name = name_attr.text
-				href = href_attr[0][1]
 				d['_type'] = 'video'
 				d['mode'] = 'lib3satHtmlPlay'
 				d['_name'] = name
@@ -259,13 +253,12 @@ def lib3satHtmlPlay(url = None):
 	if url is None:
 		url = params['url']
 	response = libMediathek.getUrl(url)
-	soup = bs.BeautifulSoup(response)
-	playerbox = soup.find('div', {'class': (lambda(x): x.find('b-playerbox')>=0)})
+	soup = bs.BeautifulSoup(response, 'html.parser')
+	playerbox = soup.find('div', {'class': 'b-playerbox'})
 	if not (playerbox is None) and (hasattr(playerbox,'attrs')):
-		jsb_arr = filter(lambda(x): x[0]=='data-zdfplayer-jsb', playerbox.attrs)
-		if len(jsb_arr) > 0:
-			json_str = jsb_arr[0][1]
-			jsb = json.loads(json_str)
+		jsb_str = playerbox.attrs.get('data-zdfplayer-jsb', None)
+		if not (jsb_str is None):
+			jsb = json.loads(jsb_str)
 			content_link = jsb['content']
 			api_token = jsb['apiToken']
 		 	content_response = getU(content_link, api_token)
