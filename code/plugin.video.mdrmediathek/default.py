22ad2b4462aea1901376e4c30eae95c95b6dc796
68000a <44293414+68000a@users.noreply.github.com>
2019-10-01 20:48:31 +0200

    MDR Mediathek - 1.1.4 MDR-Mediathek komplett in die ARD-Mediathek umgezogen siehe: https://www.mdr.de/presse/unternehmen/mdr-mediathek-in-die-ard-mediathek-umgezogen-100.html

--- code/plugin.video.mdrmediathek/default.py
+++ code/plugin.video.mdrmediathek/default.py
@@ -1,4 +1,14 @@
 ﻿# -*- coding: utf-8 -*-
+import xbmc
+import xbmcgui
+import libmediathek3 as libMediathek
 import libmdr
 
-libmdr.list()
+# libmdr.list()
+
+libMediathek.endOfDirectory()
+dialog = xbmcgui.Dialog()
+title = 'MDR Mediathek'
+text = 'Gem. Pressemitteilung sind die Inhalte der MDR-Mediathek seit 11.09.2019 ausschließlich über die ARD-Mediathek abrufbar.'
+dialog.ok(title, text)
+xbmc.executebuiltin('XBMC.ActivateWindow(Home)')
