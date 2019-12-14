9a2a4a0824eee5dfc507cdde99c12d2145db7349
68000a <44293414+68000a@users.noreply.github.com>
2019-12-14 15:32:23 +0100

    MDR Mediathek - 1.1.5 Abhängigkeit "libmdr" entfernt

--- code/plugin.video.mdrmediathek/default.py
+++ code/plugin.video.mdrmediathek/default.py
@@ -2,7 +2,7 @@
 import xbmc
 import xbmcgui
 import libmediathek3 as libMediathek
-import libmdr
+import platform
 
 # libmdr.list()
 
@@ -10,5 +10,6 @@ libMediathek.endOfDirectory()
 dialog = xbmcgui.Dialog()
 title = 'MDR Mediathek'
 text = 'Gem. Pressemitteilung sind die Inhalte der MDR-Mediathek seit 11.09.2019 ausschließlich über die ARD-Mediathek abrufbar.'
+text = text + ' [Py' + platform.python_version() + ']'
 dialog.ok(title, text)
 xbmc.executebuiltin('XBMC.ActivateWindow(Home)')
