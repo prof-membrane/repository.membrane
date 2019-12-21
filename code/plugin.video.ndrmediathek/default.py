3281de2405a961506e76a4951fa32aaa4d8d0a1d
68000a <44293414+68000a@users.noreply.github.com>
2019-12-21 15:28:34 +0100

    NDR Mediathek - 1.1.4 NDR-Mediathek komplett in die ARD-Mediathek umgezogen siehe: https://www.presseportal.de/pm/6561/4464810

--- code/plugin.video.ndrmediathek/default.py
+++ code/plugin.video.ndrmediathek/default.py
@@ -1,4 +1,15 @@
 ﻿# -*- coding: utf-8 -*-
-import libndr
+import xbmc
+import xbmcgui
+import libmediathek3 as libMediathek
+import platform
 
-libndr.list()
+# libndr.list()
+
+libMediathek.endOfDirectory()
+dialog = xbmcgui.Dialog()
+title = 'NDR Mediathek'
+text = 'Gem. Pressemitteilung sind die Inhalte der NDR-Mediathek seit 11.12.2019 ausschließlich über die ARD-Mediathek abrufbar.'
+text = text + ' [Py' + platform.python_version() + ']'
+dialog.ok(title, text)
+xbmc.executebuiltin('XBMC.ActivateWindow(Home)')
