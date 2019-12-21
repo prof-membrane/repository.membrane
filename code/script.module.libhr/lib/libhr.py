d1ef55807e8c9f8e8f1b8dd16a2933bb6afa8ea0
68000a <44293414+68000a@users.noreply.github.com>
2019-12-21 13:40:39 +0100

    Schönheitskorrektur

--- code/script.module.libhr/lib/libhr.py
+++ code/script.module.libhr/lib/libhr.py
@@ -94,7 +94,6 @@ def libHrPlay():
 	return result
 
 def headUrl(url):#TODO: move to libmediathek3
-	libMediathek.log(url)
 	req = Request(url)
 	req.get_method = lambda : 'HEAD'
 	req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; rv:25.0) Gecko/20100101 Firefox/25.0')
