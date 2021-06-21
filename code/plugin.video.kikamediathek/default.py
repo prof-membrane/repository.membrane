90df50dee54fa59a97e51f8538df0da775791f72
68000a <44293414+68000a@users.noreply.github.com>
2021-06-21 10:31:48 +0200

    KiKa Mediathek 2.0.5 KiKa Mediathek b.a.w. nicht verfügbar

--- code/plugin.video.kikamediathek/default.py
+++ code/plugin.video.kikamediathek/default.py
@@ -1,4 +1,16 @@
 ﻿# -*- coding: utf-8 -*-
 import libkika
 
-libkika.list()
+import xbmc
+import xbmcgui
+import libmediathek3 as libMediathek
+import platform
+
+# libkika.list()
+
+libMediathek.endOfDirectory()
+dialog = xbmcgui.Dialog()
+title = 'KiKa Mediathek'
+text = 'Die KiKa Mediathek ist momentan nicht verfügbar. Bitte nutzen Sie stattdessen die ZDFtivi Mediathek.'
+dialog.ok(title, text)
+xbmc.executebuiltin('XBMC.ActivateWindow(Home)')
