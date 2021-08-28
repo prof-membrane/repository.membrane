fae3e7583d4ab8c2d487027bb4cc830928f4407f
68000a <44293414+68000a@users.noreply.github.com>
2021-08-28 13:51:37 +0200

    Das Erste Mediathek - 1.1.5 Das Erste Mediathek komplett in die ARD-Mediathek umgezogen siehe: https://www.daserste.de/specials/service/app-102.html

--- code/plugin.video.daserstemediathek/default.py
+++ code/plugin.video.daserstemediathek/default.py
@@ -1,4 +1,14 @@
 ﻿# -*- coding: utf-8 -*-
+import xbmc
+import xbmcgui
+import libmediathek3 as libMediathek
 import libdaserste
 
-libdaserste.list()
+# libdaserste.list()
+
+libMediathek.endOfDirectory()
+dialog = xbmcgui.Dialog()
+title = 'Das Erste Mediathek'
+text = 'Gem. Pressemitteilung sind die Inhalte dieser Mediathek seit 28.01.2021 ausschließlich über die ARD-Mediathek abrufbar.'
+dialog.ok(title, text)
+xbmc.executebuiltin('XBMC.ActivateWindow(Home)')
