26617a070a2ae23980942b239fad3f1a32caad5f
68000a <44293414+68000a@users.noreply.github.com>
2022-06-03 16:20:59 +0200

    ARTE Mediathek - 1.0.10 / libarte 1.0.25 ARTE-Mediathek reaktiviert, alle Streams können wieder abgespielt werden

---
+++ code/script.module.libarte/lib/libarte.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+import sys
+import libartejsonparser as libArteJsonParser
+import libmediathek3 as libMediathek
+
+translation = libMediathek.getTranslation
+params = libMediathek.get_params()
+
+def list():
+	return libMediathek.list(modes, 'libArteListMain', 'libArtePlay')
+
+def libArteListMain():
+	l = []
+	l.append({'_name': translation(31031), 'mode': 'libArteListVideos',	'_type': 'dir', 'url':'/zones/listing_MOST_VIEWED?limit=20'}) # Meistgesehen
+	l.append({'_name': translation(31032), 'mode': 'libArteListMagazines',	'_type': 'dir'}) # Sendungen A-Z
+	l.append({'_name': translation(31033), 'mode': 'libArteListDate',	'_type': 'dir'}) # Die Woche
+	l.append({'_name': translation(31039), 'mode': 'libArteListSearch', '_type': 'dir'}) # Suche
+	return l
+
+def libArteListCollection():
+	return libArteJsonParser.getCollection(params['url'])
+
+def libArteListVideos():
+	return libArteJsonParser.getVideos(params['url'])
+
+def libArteListMagazines():
+	return libArteJsonParser.getMagazines()
+
+def libArteListDate():
+	return libMediathek.populateDirDate('libArteListDateVideos')
+
+def libArteListDateVideos():
+	return libArteJsonParser.getDate(params['yyyymmdd'])
+
+def libArteListSearch():
+	search_string = libMediathek.getSearchString()
+	return libArteJsonParser.getSearch(search_string) if search_string else None
+
+def libArtePlay():
+	result = libArteJsonParser.getVideoUrl(params['url'], params['documentId'])
+	result = libMediathek.getMetadata(result)
+	return result
+
+modes = {
+	'libArteListMain': libArteListMain,
+	'libArteListCollection':libArteListCollection,
+	'libArteListVideos': libArteListVideos,
+	'libArteListMagazines': libArteListMagazines,
+	'libArteListDate': libArteListDate,
+	'libArteListDateVideos': libArteListDateVideos,
+	'libArteListSearch': libArteListSearch,
+	'libArtePlay': libArtePlay,
+}
+
